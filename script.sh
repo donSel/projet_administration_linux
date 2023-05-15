6a3ddb037fcaec2b5793523e6718374e8cd63255
Mickaël Neroda <mickael.neroda@isen-ouest.yncrea.fr>
2023-05-15 14:31:29 +0200

    création script.sh + fichier account.csv

--- script.sh
+++ script.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Chemin vers le fichier accounts.csv
-FILE="accounts.csv"
-
-# Boucle sur chaque ligne du fichier
-while IFS=';' read -r name surname mail password; do
-    let name = $name
-    let surname = $surname
-    let mail = $mail
-    let password = $password
-done < "$FILE"
 
 
 
