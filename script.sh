e190d982188aebf8fdd2b769f2466d4e6bc1dc97
Mickaël Neroda <mickael.neroda@isen-ouest.yncrea.fr>
2023-05-26 10:49:40 +0200

    ajout d'envoi de mails de (pb mdp tjrs présent)

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# --------------------- / INSTALLATION DE BASE \ ---------------------
+# --------------------------------[[CREATION DES DOSSIER ET UTILISATEURS]--------------------------------[
 
 # Création du fichier shared appartenant à root 
 mkdir /home/shared
@@ -8,6 +8,20 @@ mkdir /home/shared
 # Chemin vers le fichier accounts.csv
 FILE="accounts.csv"
 
+# Demande des détails du serveur SMTP
+echo "Veuillez saisir votre adresse de serveur smtp (pour Outlook : smtp.office365.com) :"
+read smtp_server
+
+echo "Veuillez saisir le port de votre serveur smtp (pour Outlook : 587) :"
+read smtp_port
+
+echo "Veuillez rentrer l'adresse email de l'expéditeur :"
+read from_email
+
+echo "Veuillez saisir le mot de passe de cette adresse e-mail :"
+read smtp_password
+
+    
 # Boucle de lecture sur le fichier account.csv (excepté la première)
 tail -n +2 "$FILE" | while IFS=';' read -r name surname mail password; do
 
@@ -34,6 +48,17 @@ tail -n +2 "$FILE" | while IFS=';' read -r name surname mail password; do
     chown "$username" "/home/shared/$username"
     chmod 755 "/home/shared/$username"
     
+    # --------------------------------[ENVOI DE MAIL]--------------------------------
+    
+    # Spécifiez les détails de l'e-mail
+    body="Bonjour $name $surname,\n\nVotre compte utilisateur a été créé avec succès.\nVotre login : $username\nVotre mot de passe de connexion est : $password\n\n/!\ Attention ce mot de passe devra être changé lors de votre première connexion\n\n'Cordialement,\n\nL'équipe de support"
+          
+    # Envoi de l'e-mail via le serveur SMTP
+     
+    subject="Votre compte a été créé"
+    #mail --subject "Votre compte a été créé" --exec "set sendmail=smtp://${from_email/@/%40}:${smtp_password/@/%40}@$smtp_server:$smtp_port" --append "From:$from_email" $mail <<< "$body"
+    ssh -n aaugus25@10.30.48.100 "mail --subject \"Votre compte a été créé\" --exec \"set sendmail=smtp://${from_email/@/%40}:${smtp_password/@/%40}@$smtp_server:$smtp_port\" --append \"From:$from_email\" $mail <<< \"$body\" "
+
 done 
 
 # Mise a jour des droits du fichier shared
